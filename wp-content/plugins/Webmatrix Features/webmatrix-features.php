58d1b6c8b4b3ce57265ef11f0b975134f36c3326
Webmatrix-Solutions <webmatrixsolutions1@gmail.com>
2022-10-29 05:44:52 +0530

    wp core files added

---
+++ wp-content/plugins/Webmatrix Features/webmatrix-features.php
@@ -0,0 +1,25 @@
+<?php 
+/**
+ * Webmatrix Features
+ *
+ * @package           webmatrix
+ * @author            Abir
+ * @copyright         2022 Webmatrix Solutions
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Webmatrix Features
+ * Plugin URI:        https://wmxsolutions.com/
+ * Description:       This plugin will implement all the basic features of the website,
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Abir
+ * Author URI:        https://wmxsolutions.com
+ * Text Domain:       webmatrix
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Update URI:        https://wmxsolutions.com/webmatrix-features/
+ */
+
+?>
